872b9e702690c7d37ad8aa0bc718ac6b185029f7
IrfanHidayat <109232345+IfranHidayat@users.noreply.github.com>
2022-07-14 00:46:44 +0800

    Memperbaki template dan Menambahkan fiture user

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,6 +21,16 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
-		
+	}
+
+	public function about()
+	{
+	// fungsi untuk me-load view about.php
+		$this->load->view('about');
+	}
+	public function profile()
+	{
+	// fungsi untuk me-load view contact.php
+		$this->load->view('profile');
 	}
 }
